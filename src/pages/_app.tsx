f8bb1c71eedf8fd3754a86e24ae62d95a0b5a930
VitorMendesco <vitormendesco.js@gmail.com>
2023-02-21 23:19:56 -0300

    feat(app): implementing emotion cache to ssr stylization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+
+import { createEmotionCache } from '@utils/styles/createEmotionCache'
+
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  )
 }

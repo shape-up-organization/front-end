07e6d221ef34d19bba04f3da236622af850f523f
VitorMendesco <vitormendesco.js@gmail.com>
2023-03-05 20:02:38 -0300

    refactor(modal): transferring login modal structure to a generic one

--- src/components/Modal/LoginModal.jsx
+++ src/components/Modal/LoginModal.jsx
@@ -1,19 +1,12 @@
 import P from 'prop-types'
 import { useState } from 'react'
 
-import { Divider } from '@components/Divider'
-import { ExternalButton } from '@components/ExternalButton'
 import { Link } from '@components/Link'
-import Close from '@mui/icons-material/Close'
+import { Modal } from '@components/Modal/Modal'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import {
-  Box,
   Button,
-  Container,
-  Dialog,
-  DialogContent,
-  DialogTitle,
   Grid,
   IconButton,
   InputAdornment,
@@ -21,96 +14,77 @@ import {
   Typography,
 } from '@mui/material'
 
-const LoginModal = ({ open, handleClose }) => {
+const Content = () => {
   const [showPassword, setShowPassword] = useState(false)
 
   return (
-    <Dialog
-      fullWidth
-      open={open}
-      onClose={handleClose}
-      maxWidth="xs"
-      PaperProps={{
-        sx: { padding: theme => theme.spacing(2, 0, 3) },
-      }}
-    >
-      <DialogTitle align="center" justifyContent="center">
-        <Typography color="primary" component="p" fontWeight="bold" variant="h4">
-          Faça Login
-        </Typography>
-        <IconButton
-          aria-label="Close login modal"
-          onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            right: theme => theme.spacing(2),
-            top: theme => theme.spacing(2),
+    <Grid container justifyContent="center" rowSpacing={2} paddingTop={1}>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          label="E-mail"
+          name="email"
+          type="email"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          label="Senha"
+          name="password"
+          type={showPassword ? 'text' : 'password'}
+          variant="outlined"
+          onMouseEnter={event => event.preventDefault()}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={() =>
+                    setShowPassword(prevShowPassword => !prevShowPassword)
+                  }
+                  onMouseDown={event => event.preventDefault()}
+                >
+                  {showPassword ? (
+                    <VisibilityOff fontSize="small" />
+                  ) : (
+                    <Visibility fontSize="small" />
+                  )}
+                </IconButton>
+              </InputAdornment>
+            ),
           }}
-        >
-          <Close />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent>
-        <Container maxWidth="xs">
-          <Grid container justifyContent="center" rowSpacing={2} paddingTop={1}>
-            <Grid item xs={12}>
-              <TextField fullWidth label="E-mail" name="email" type="email" variant="outlined" />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Senha"
-                name="password"
-                type={showPassword ? 'text' : 'password'}
-                variant="outlined"
-                onMouseEnter={event => event.preventDefault()}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="Toggle password visibility"
-                        onClick={() => setShowPassword(prevShowPassword => !prevShowPassword)}
-                        onMouseDown={event => event.preventDefault()}
-                      >
-                        {showPassword ? (
-                          <VisibilityOff fontSize="small" />
-                        ) : (
-                          <Visibility fontSize="small" />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid item xs={5}>
-              <Button fullWidth size="large" variant="contained">
-                <Typography fontWeight="bold" textTransform="none">
-                  ENTRAR
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item textAlign="center" xs={12}>
-              <Link>
-                <Typography fontWeight="bold" variant="caption">
-                  Esqueceu sua senha?
-                </Typography>
-              </Link>
-            </Grid>
-          </Grid>
-          <Divider text="OU" />
-          <Box alignItems="center" display="flex" flexDirection="column">
-            <Grid container justifyContent="center" gap={2}>
-              {['google', 'facebook', 'twitter'].map((site, index) => (
-                <Grid item key={index} xs={12}>
-                  <ExternalButton type={site} />
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-        </Container>
-      </DialogContent>
-    </Dialog>
+        />
+      </Grid>
+      <Grid item xs={5}>
+        <Button fullWidth size="large" variant="contained">
+          <Typography fontWeight="bold" textTransform="none">
+            ENTRAR
+          </Typography>
+        </Button>
+      </Grid>
+      <Grid item textAlign="center" xs={12}>
+        <Link>
+          <Typography fontWeight="bold" variant="caption">
+            Esqueceu sua senha?
+          </Typography>
+        </Link>
+      </Grid>
+    </Grid>
+  )
+}
+
+const LoginModal = ({ open, handleClose }) => {
+  const title = 'Faça Login'
+
+  return (
+    <Modal
+      content={<Content />}
+      handleClose={handleClose}
+      open={open}
+      title={title}
+    />
   )
 }
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,22 @@
 import P from 'prop-types'
-import { useState } from 'react'
 
 import { Divider } from '@components/Divider'
-import { ExternalButton } from '@components/ExternalButton'
-import { Link } from '@components/Link'
+import { ExternalButtons } from '@components/Modal/ExternalButtons'
 import Close from '@mui/icons-material/Close'
-import Visibility from '@mui/icons-material/Visibility'
-import VisibilityOff from '@mui/icons-material/VisibilityOff'
 import {
   Box,
-  Button,
   Container,
   Dialog,
   DialogContent,
   DialogTitle,
-  Grid,
   IconButton,
-  InputAdornment,
-  TextField,
   Typography,
 } from '@mui/material'
 
-const Modal = ({ handleClose, open, title }) => {
-  const [showPassword, setShowPassword] = useState(false)
+const Modal = ({ content, direction, handleClose, open, title }) => {
+  const contentDirection = direction === 'horizontal' ? 'row' : 'column'
+  const dividerDirection =
+    contentDirection === 'row' ? 'vertical' : 'horizontal'
 
   return (
     <Dialog
@@ -35,7 +29,12 @@ const Modal = ({ handleClose, open, title }) => {
       }}
     >
       <DialogTitle align="center" justifyContent="center">
-        <Typography color="primary" component="p" fontWeight="bold" variant="h4">
+        <Typography
+          color="primary"
+          component="p"
+          fontWeight="bold"
+          variant="h4"
+        >
           {title}
         </Typography>
         <IconButton
@@ -51,62 +50,14 @@ const Modal = ({ handleClose, open, title }) => {
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        <Container maxWidth="xs">
-          <Grid container justifyContent="center" rowSpacing={2} paddingTop={1}>
-            <Grid item xs={12}>
-              <TextField fullWidth label="E-mail" name="email" type="email" variant="outlined" />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Senha"
-                name="password"
-                type={showPassword ? 'text' : 'password'}
-                variant="outlined"
-                onMouseEnter={event => event.preventDefault()}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="Toggle password visibility"
-                        onClick={() => setShowPassword(prevShowPassword => !prevShowPassword)}
-                        onMouseDown={event => event.preventDefault()}
-                      >
-                        {showPassword ? (
-                          <VisibilityOff fontSize="small" />
-                        ) : (
-                          <Visibility fontSize="small" />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid item xs={5}>
-              <Button fullWidth size="large" variant="contained">
-                <Typography fontWeight="bold" textTransform="none">
-                  ENTRAR
-                </Typography>
-              </Button>
-            </Grid>
-            <Grid item textAlign="center" xs={12}>
-              <Link>
-                <Typography fontWeight="bold" variant="caption">
-                  Esqueceu sua senha?
-                </Typography>
-              </Link>
-            </Grid>
-          </Grid>
-          <Divider text="OU" />
+        <Container
+          maxWidth="xs"
+          sx={{ display: 'flex', flexDirection: contentDirection }}
+        >
+          {content}
+          <Divider direction={dividerDirection} text="OU" />
           <Box alignItems="center" display="flex" flexDirection="column">
-            <Grid container justifyContent="center" gap={2}>
-              {['google', 'facebook', 'twitter'].map((site, index) => (
-                <Grid item key={index} xs={12}>
-                  <ExternalButton type={site} />
-                </Grid>
-              ))}
-            </Grid>
+            <ExternalButtons />
           </Box>
         </Container>
       </DialogContent>
@@ -115,12 +66,15 @@ const Modal = ({ handleClose, open, title }) => {
 }
 
 Modal.propTypes = {
+  content: P.element.isRequired,
+  direction: P.oneOf(['horizontal', 'vertical']),
   handleClose: P.func.isRequired,
   open: P.bool.isRequired,
   title: P.bool,
 }
 
 Modal.defaultProps = {
+  direction: 'vertical',
   title: '',
 }
 

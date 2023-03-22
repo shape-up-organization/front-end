1cae4465726e335219ba7d9722b9161fc85850f6
VitorMendesco <vitormendesco.js@gmail.com>
2023-03-22 20:25:26 -0300

    feat(landing page): implementing responsiveness

--- src/components/pages/LandingPage/LandingPage.jsx
+++ src/components/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
-import { Grid } from '@mui/material'
+import { Grid, useMediaQuery, useTheme } from '@mui/material'
+
 import { LoginModal } from '@molecules/LoginModal'
 import { SignupModal } from '@molecules/SignupModal'
 import { Banner } from '@organisms/Banner'
@@ -10,6 +11,8 @@ import { Header } from '@organisms/Header'
 const LandingPage = () => {
   const [isLoginOpen, setIsLoginOpen] = useState(false)
   const [isSignupOpen, setIsSignupOpen] = useState(false)
+
+  const lessThanMedium = useMediaQuery(useTheme().breakpoints.down('md'))
 
   const switchModal = () => {
     setIsLoginOpen(prevIsLoginOpen => !prevIsLoginOpen)
@@ -30,13 +33,29 @@ const LandingPage = () => {
         <Grid container item xs={10} minHeight="48vh" alignItems="center">
           <Banner handleOpenSignup={handleOpenModals.handleOpenSignup} />
         </Grid>
-        <Grid container item xs={10}>
-          <Grid item display="flex" alignItems="flex-end" py={4} xs={12} lg={6}>
-            <Grid item xs={12} lg={8}>
-              <Footer />
+        {!lessThanMedium && (
+          <Grid container item xs={10}>
+            <Grid
+              item
+              alignItems="flex-end"
+              display="flex"
+              py={4}
+              xs={12}
+              lg={6}
+            >
+              <Grid
+                container
+                item
+                alignItems="center"
+                justifyContent="space-between"
+                xs={12}
+                lg={8}
+              >
+                <Footer showCircles />
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        )}
       </Grid>
       <LoginModal
         isOpen={isLoginOpen}
